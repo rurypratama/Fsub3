1467f1a76ae6e6e2eb0404ef46490323060302a2
Zaen67 <96893984+Zaen67@users.noreply.github.com>
2022-04-16 00:09:09 +0700

    Update helper_func.py

--- helper_func.py
+++ helper_func.py
@@ -14,7 +14,7 @@ from pyrogram.errors.exceptions.bad_request_400 import UserNotParticipant
 async def is_subscribed(filter, client, update):
     if not FORCE_SUB_CHANNEL: 
         return True
-     if not FORCE_SUB_CHANNEL: 
+    if not FORCE_SUB_CHANNEL: 
         return True
     if not FORCE_SUB_GROUP:
         return True
@@ -24,8 +24,7 @@ async def is_subscribed(filter, client, update):
     try:
         member = await client.get_chat_member(chat_id=FORCE_SUB_CHANNEL, user_id=user_id)
         member = await client.get_chat_member(chat_id=FORCE_SUB_GROUP, user_id=user_id)
-        member = await client.get_chat_member(chat_id=FORCE_SUB_CHANNEL1, user_id=user_id)
-        member = await client.get_chat_member(chat_id=FORCE_SUB_GROUP1, user_id=user_id)    
+        member = await client.get_chat_member(chat_id=FORCE_SUB_CHANNEL1, user_id=user_id)   
     except UserNotParticipant:
         return False
 
