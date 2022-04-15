ad1fc67195bec3c0c2d15b6d296572654cf90a38
Zaen67 <96893984+Zaen67@users.noreply.github.com>
2022-04-16 00:08:34 +0700

    Update helper_func.py

--- helper_func.py
+++ helper_func.py
@@ -12,7 +12,9 @@ from pyrogram.errors import FloodWait
 from pyrogram.errors.exceptions.bad_request_400 import UserNotParticipant
 
 async def is_subscribed(filter, client, update):
-    if not FORCE_SUB_CHANNEL1: 
+    if not FORCE_SUB_CHANNEL: 
+        return True
+     if not FORCE_SUB_CHANNEL: 
         return True
     if not FORCE_SUB_GROUP:
         return True
